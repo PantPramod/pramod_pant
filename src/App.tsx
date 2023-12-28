2a8abca20292efc4fa04aede8aea98997089a036
Pramod Pant <pramodpant100@gmail.com>
2023-12-28 08:32:19 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="flex flex-col min-h-screen w-full items-center justify-center font-raleway px-4">
+
+      <h1 className="text-2xl sm:text-3xl md:text-5xl text-center font-semibold  text-gray-700 ">Redefining Web Excellence</h1>
+      <p className="text-center mt-1 text-xs sm:text-sm md:text-xl font-semibold text-gray-500">Where Innovation Meets User Experience</p>
+      
+      <button className="mt-3 font-semibold bg-gray-800 text-white rounded-[4px] sm:rounded-md px-4 sm:px-6  py-1 sm:py-2 hover:bg-black">Get in Touch!</button>
+    </div>
+  )
+}
+
+export default App
