8ae080b0576f02c8cb974a0ccdaff857a9ac4f72
Pramod Pant <pramodpant100@gmail.com>
2024-02-09 11:28:13 +0530

    realtime memory game

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ContactForm from "./components/ContactForm"
 import chatapp from './assets/chatapp.png'
 import recipe from './assets/recipe.png'
+import memorygame from './assets/memorygame.png'
 
 const App = () => {
   return (
@@ -56,6 +57,27 @@ const App = () => {
             <div className="flex items-center justify-between px-2 pt-2 mt-2 mb-2">
               <a href="https://github.com/PantPramod/recepies-app" target="_blank" className=" text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
               <a href="https://recepies-app-theta.vercel.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
+            </div>
+          </div>
+
+          <div className="w-[300px] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200 self-stretch">
+
+            <img
+              src={memorygame}
+              alt="chat app"
+              className="border-b border-b-gray-400 min-h-[190px] object-contain"
+              width={300}
+              height={300}
+
+            />
+            <p className="font-bold uppercase text-center mt-2">Realtime Memory Game</p>
+            <p className="text-gray-600 text-xs px-2 flex gap-x-2 font-bold mt-2">
+              <span>Technology:</span>
+              <span>   React JS, Node JS, Express JS, TypeScript, Socket.Io</span>
+            </p>
+            <div className="flex items-center justify-between px-2 pt-2 mt-2 mb-2">
+              <a href="https://github.com/PantPramod/real-time-memory-game" target="_blank" className=" text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
+              <a href="https://memorygame124.netlify.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
             </div>
           </div>
         </div>
