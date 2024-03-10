27437d57f88c00dd0cc11876fbfbbfb95dbabd0b
Pramod Pant <pramodpant100@gmail.com>
2024-03-10 17:58:19 +0530

    project added

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,45 @@ import ContactForm from "./components/ContactForm"
 import chatapp from './assets/chatapp.png'
 import recipe from './assets/recipe.png'
 import memorygame from './assets/memorygame.png'
+import ecommerce from "./assets/ecommerce.png"
 import HeroSection from "./components/HeroSection"
 
 const App = () => {
   return (
     <div className="font-raleway scroll-smooth">
       <HeroSection />
- 
+
       <div className="px-10">
         <h2 className='text-center text-2xl md:text-3xl font-bold mb-4 text-gray-700 py-10'>Projects</h2>
 
-        <div className="flex gap-10 items-center flex-wrap mt-10 justify-evenly">
-          <div className="w-[300px] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200">
+        <div className="flex gap-15  flex-wrap mt-10 justify-between gap-y-20 items-stretch">
+
+          <div className="mx-auto sm:mx-[unset] max-w-[300px] w-full sm:w-[45%] md:w-[30%] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200 self-stretch">
 
             <img
-              src={chatapp}
-              alt="chat app"
-              className="border-b border-b-gray-400 "
+              src={ecommerce}
+              alt="E Commerce App"
+              className="border-b border-b-gray-400 min-h-[190px] object-contain"
               width={300}
               height={300}
 
             />
-            <p className="font-bold uppercase text-center mt-2">Chat App</p>
+            <p className="font-bold uppercase text-center mt-2">Artisan Crafts</p>
             <p className="text-gray-600 text-xs px-2 flex gap-x-2 font-bold mt-2">
               <span>Technology:</span>
-              <span>  React JS , Node JS, Express JS, Socket.IO, MongoDB</span>
+              <span>   React JS, Next JS, Node JS, Express JS, Mongoose, TypeScript</span>
             </p>
             <div className="flex items-center justify-between px-2 pt-2 mt-2 mb-2">
-              <a href="https://github.com/PantPramod/chat-app" target="_blank" className=" text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
-              <a href="https://chatappbypramod.netlify.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
+              <a href="https://github.com/PantPramod/artisan-creafts" target="_blank" className="text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
+              <a href="#" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
             </div>
           </div>
 
-          <div className="w-[300px] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200 self-stretch">
+
+
+
+
+          <div className="mx-auto sm:mx-[unset] max-w-[300px] w-full sm:w-[45%] md:w-[30%] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200 self-stretch">
 
             <img
               src={recipe}
@@ -55,7 +61,7 @@ const App = () => {
             </div>
           </div>
 
-          <div className="w-[300px] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200 self-stretch">
+          <div className="mx-auto sm:mx-[unset] max-w-[300px] w-full sm:w-[45%] md:w-[30%] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200 self-stretch">
 
             <img
               src={memorygame}
@@ -75,6 +81,32 @@ const App = () => {
               <a href="https://memorygame124.netlify.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
             </div>
           </div>
+          <div className="mx-auto sm:mx-[unset] max-w-[300px] w-full sm:w-[45%] md:w-[30%] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200">
+
+            <img
+              src={chatapp}
+              alt="chat app"
+              className="border-b border-b-gray-400 "
+              width={300}
+              height={300}
+
+            />
+            <p className="font-bold uppercase text-center mt-2">Chat App</p>
+            <p className="text-gray-600 text-xs px-2 flex gap-x-2 font-bold mt-2">
+              <span>Technology:</span>
+              <span>  React JS , Node JS, Express JS, Socket.IO, MongoDB</span>
+            </p>
+            <div className="flex items-center justify-between px-2 pt-2 mt-2 mb-2">
+              <a href="https://github.com/PantPramod/chat-app" target="_blank" className=" text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
+              <a href="https://chatappbypramod.netlify.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
+            </div>
+          </div>
+
+
+
+
+
+
         </div>
       </div>
       <ContactForm />
