f25ace053d0bcbd7b3d2b1c99cca3dc15562d9a6
Pramod Pant <pramodpant100@gmail.com>
2024-05-08 17:27:22 +0530

    added one utility project

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import chatapp from './assets/chatapp.png'
 import recipe from './assets/recipe.png'
 import memorygame from './assets/memorygame.png'
 import ecommerce from "./assets/ecommerce.png"
+import utility from './assets/utility.png'
 //@ts-ignore
 import HeroSection from "./components/HeroSection.jsx"
 import "./App.css"
@@ -102,6 +103,28 @@ const App = () => {
             <div className="flex items-center justify-between px-2 pt-2 mt-2 mb-2">
               <a href="https://github.com/PantPramod/chat-app" target="_blank" className=" text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
               <a href="https://chatappbypramod.netlify.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
+            </div>
+          </div>
+
+
+          <div className="mx-auto sm:mx-[unset] max-w-[300px] w-full sm:w-[45%] md:w-[30%] shadow-2xl rounded-xl overflow-hidden border border-gray-400 pb-2 bg-gray-200">
+
+            <img
+              src={utility}
+              alt="utility app"
+              className="border-b border-b-gray-400 "
+              width={300}
+              height={300}
+
+            />
+            <p className="font-bold uppercase text-center mt-2">Utility App</p>
+            <p className="text-gray-600 text-xs px-2 flex gap-x-2 font-bold mt-2">
+              <span>Technology:</span>
+              <span>  React JS , Node JS, Express JS, Puppeteer</span>
+            </p>
+            <div className="flex items-center justify-between px-2 pt-2 mt-2 mb-2">
+              <a href="https://github.com/PantPramod/utility-app" target="_blank" className=" text-white border-2 w-[104px] flex items-center justify-center bg-blue-600 border-blue-600 hover:scale-105 transition-all ease-in-out duration-300 p-2  rounded-full  font-semibold text-center text-xs">Source code</a>
+              <a href="https://utility-app-nine.vercel.app/" target="_blank" className="border-2 border-purple-600 w-[104px] flex items-center justify-center p-2 hover:scale-105 transition-all ease-in-out duration-300  rounded-full  text-white bg-purple-600 font-semibold text-right text-xs">Demo Link</a>
             </div>
           </div>
 
