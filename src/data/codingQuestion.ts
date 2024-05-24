07ff8ab24bc1e7d47094bfa0d7a006387d8ea809
Pramod Pant <pramodpant100@gmail.com>
2024-05-24 21:33:02 +0530

    "Added import statement, modified JSX structure, and added mapping of codingQuestion data to display questions and iframes."

---
+++ src/data/codingQuestion.ts
@@ -0,0 +1,16 @@
+const codingQuestion = [
+    {
+        que: "Write a JS Code to remove duplicate character from string .",
+        ans: "https://codesandbox.io/p/devbox/remove-duplicate-from-string-cldy8l?embed=1&file=%2Fsrc%2Findex.mjs",
+        category: "string"
+    },
+    {
+        que: "Write a JS Function to reverse each world in a string.",
+        ans:"https://codesandbox.io/p/devbox/75mggj?migrateFrom=r7fyyf",
+        category:"string"
+    }
+
+
+]
+
+export default codingQuestion
--- src/pages/CodingProblems.tsx
+++ src/pages/CodingProblems.tsx
@@ -1,9 +1,23 @@
-
+import codingQuestion from "../data/codingQuestion"
 
 const CodingProblems = () => {
   return (
-    <div>
-      
+    <div className="w-[90%] mx-auto">
+      <p className="text-center text-2xl md:text-3xl font-bold mb-4 text-gray-700 py-5">Coding Questions</p>
+      <div className="py-10 ">
+        {
+          codingQuestion.map((item, index) => <div key={item?.que} className="mt-10">
+            <p><strong>{index + 1}.</strong>{" "}{item?.que}</p>
+            <iframe src={item.ans}
+         
+              title="JS coding question"
+              allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+              sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+              className="w-[100%] mt-5 h-[400px] rounded"
+            ></iframe>
+          </div>)
+        }
+      </div>
     </div>
   )
 }
