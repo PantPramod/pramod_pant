a2ad027419817cc4a9776397384a7a68b1cd7257
Pramod Pant <pramodpant100@gmail.com>
2024-05-25 17:07:36 +0530

    "Added three new coding questions to the codingQuestion array in codingQuestion.ts"

--- src/data/codingQuestion.ts
+++ src/data/codingQuestion.ts
@@ -82,6 +82,70 @@ const codingQuestion = [
         convertToCamelCase("Hello World From JavaScript")
         `,
         category: "string"
+    },
+    {
+        que: "Write a JavaScript function to uncommelize a string.",
+        category: "string",
+        code: `//Write a JavaScript function to uncommelize a string.
+
+        function unCamelize(str) {
+          let arr = str.split("");
+        
+          let dummyArr = [];
+          for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === arr[i].toUpperCase()) {
+              dummyArr.push(" ")
+              dummyArr.push(arr[i].toLowerCase());
+            } else {
+              dummyArr.push(arr[i]);
+            }
+          }
+        
+          return dummyArr.join("");
+        }
+        
+        const result = unCamelize("helloWorld");
+        console.log(result);
+        //output=> hello world
+        `,
+        ans: "https://codesandbox.io/p/sandbox/string-manipulation-9dg447?file=%2Fsrc%2Findex.mjs%3A4%2C3"
+    },
+    {
+        que: "Write a JavaScript function to concatenate a given string n times",
+        code: `//Write a JavaScript function to concatenate a given string n times
+
+        function repeat(str, n = 1) {
+          let result = "";
+        
+          for (let i = 0; i < n; i++) {
+            result += str;
+          }
+        
+          return result;
+        }
+        
+        console.log(repeat("Ha!"));
+        // output=> "Ha!"
+        
+        console.log(repeat("Ha!", 2));
+        //output=>"Ha!Ha!"
+        
+        console.log(repeat("Ha!", 3));`,
+        category: "string",
+        ans: "https://codesandbox.io/p/sandbox/string-k6ts92"
+    },
+    {
+        que: "Write a JavaScript function to insert a string within a string at a particular position (default is 1).",
+        code: `//Write a JavaScript function to insert a string within a string at a particular position (default is 1).
+        function insert(str1, str2, pos = 1) {
+          return [str1.slice(0, pos), str2, str1.slice(pos)].join("");
+        }
+        
+        let result = insert("We are doing some exercises.", "JavaScript ", 18);
+        
+        console.log(result);`,
+        category: "string",
+        ans:"https://codesandbox.io/p/sandbox/string-2-8yg7cy"
     }
 
 
