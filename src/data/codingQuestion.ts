980df212028bdd9ed45ac474420946bf9d68949b
Pramod Pant <pramodpant100@gmail.com>
2024-05-25 21:28:13 +0530

    "Updated codingQuestion.ts: reformatted code, added new questions and answers"

--- src/data/codingQuestion.ts
+++ src/data/codingQuestion.ts
@@ -1,21 +1,21 @@
 const codingQuestion = [
-    {
-        que: "Write a JS Code to remove duplicate character from string .",
-        ans: "https://codesandbox.io/p/devbox/remove-duplicate-from-string-cldy8l?embed=1&file=%2Fsrc%2Findex.mjs",
-        category: "string",
-        code: `function removeDuplicateCharacterFromString(str) {
+  {
+    que: "Write a JS Code to remove duplicate character from string .",
+    ans: "https://codesandbox.io/p/devbox/remove-duplicate-from-string-cldy8l?embed=1&file=%2Fsrc%2Findex.mjs",
+    category: "string",
+    code: `function removeDuplicateCharacterFromString(str) {
             return [...new Set(str.split(""))].join("");
           }
           
           const result = removeDuplicateCharacterFromString("Hello World");
           
           console.log(result)`
-    },
-    {
-        que: "Write a JS Function to reverse each world in a string.",
-        ans: "https://codesandbox.io/p/devbox/75mggj?migrateFrom=r7fyyf",
-        category: "string",
-        code: `function reverseWordInString(str) {
+  },
+  {
+    que: "Write a JS Function to reverse each world in a string.",
+    ans: "https://codesandbox.io/p/devbox/75mggj?migrateFrom=r7fyyf",
+    category: "string",
+    code: `function reverseWordInString(str) {
             return str
               .split(" ")
               .map((word) => word.split("").reverse().join(""))
@@ -24,32 +24,32 @@ const codingQuestion = [
           
           const result = reverseWordInString("Hello World");
           console.log(result);`
-    },
-    {
-        que: "Write a JavaScript function to capitalize the first letter of a string.",
-        code: `function capitalizeFirstLetter(str){
+  },
+  {
+    que: "Write a JavaScript function to capitalize the first letter of a string.",
+    code: `function capitalizeFirstLetter(str){
             return str.charAt(0).toUpperCase() + str.slice(1);
     }
         
     capitalizeFirstLetter("hello World in js")
         `,
-        category: `string`
+    category: `string`
 
-    },
-    {
-        que: 'Write a JavaScript function to capitalize the first letter of each word in a string.',
-        code: `function capitalizeEachWordInString(str){
+  },
+  {
+    que: 'Write a JavaScript function to capitalize the first letter of each word in a string.',
+    code: `function capitalizeEachWordInString(str){
               return str.split(" ").map(word=>word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
         }
         
         
        console.log(capitalizeEachWordInString("hello world from javascript "))
         `,
-        category: "string"
-    },
-    {
-        que: "Write a JavaScript function that takes a string with both lowercase and upper case letters as a parameter. It converts upper case letters to lower case, and lower case letters to upper case.",
-        code: `function toogleLetters(str){
+    category: "string"
+  },
+  {
+    que: "Write a JavaScript function that takes a string with both lowercase and upper case letters as a parameter. It converts upper case letters to lower case, and lower case letters to upper case.",
+    code: `function toogleLetters(str){
            return str.split('').map(letter=>{
             if(letter===" "){
                 return letter
@@ -66,11 +66,11 @@ const codingQuestion = [
         
         toogleLetters("Hello World From JavaScript")
         `,
-        category: "string"
-    },
-    {
-        que: "Write a JavaScript function to convert a string into camel case.",
-        code: `function convertToCamelCase(str){
+    category: "string"
+  },
+  {
+    que: "Write a JavaScript function to convert a string into camel case.",
+    code: `function convertToCamelCase(str){
            return str.split(" ").map((word, index)=>{
               if(index===0){
                 return word.charAt(0).toLowerCase() + word.slice(1)
@@ -81,12 +81,12 @@ const codingQuestion = [
         
         convertToCamelCase("Hello World From JavaScript")
         `,
-        category: "string"
-    },
-    {
-        que: "Write a JavaScript function to uncommelize a string.",
-        category: "string",
-        code: `//Write a JavaScript function to uncommelize a string.
+    category: "string"
+  },
+  {
+    que: "Write a JavaScript function to uncommelize a string.",
+    category: "string",
+    code: `//Write a JavaScript function to uncommelize a string.
 
         function unCamelize(str) {
           let arr = str.split("");
@@ -108,11 +108,11 @@ const codingQuestion = [
         console.log(result);
         //output=> hello world
         `,
-        ans: "https://codesandbox.io/p/sandbox/string-manipulation-9dg447?file=%2Fsrc%2Findex.mjs%3A4%2C3"
-    },
-    {
-        que: "Write a JavaScript function to concatenate a given string n times",
-        code: `//Write a JavaScript function to concatenate a given string n times
+    ans: "https://codesandbox.io/p/sandbox/string-manipulation-9dg447?file=%2Fsrc%2Findex.mjs%3A4%2C3"
+  },
+  {
+    que: "Write a JavaScript function to concatenate a given string n times",
+    code: `//Write a JavaScript function to concatenate a given string n times
 
         function repeat(str, n = 1) {
           let result = "";
@@ -131,12 +131,12 @@ const codingQuestion = [
         //output=>"Ha!Ha!"
         
         console.log(repeat("Ha!", 3));`,
-        category: "string",
-        ans: "https://codesandbox.io/p/sandbox/string-k6ts92"
-    },
-    {
-        que: "Write a JavaScript function to insert a string within a string at a particular position (default is 1).",
-        code: `//Write a JavaScript function to insert a string within a string at a particular position (default is 1).
+    category: "string",
+    ans: "https://codesandbox.io/p/sandbox/string-k6ts92"
+  },
+  {
+    que: "Write a JavaScript function to insert a string within a string at a particular position (default is 1).",
+    code: `//Write a JavaScript function to insert a string within a string at a particular position (default is 1).
         function insert(str1, str2, pos = 1) {
           return [str1.slice(0, pos), str2, str1.slice(pos)].join("");
         }
@@ -144,11 +144,65 @@ const codingQuestion = [
         let result = insert("We are doing some exercises.", "JavaScript ", 18);
         
         console.log(result);`,
-        category: "string",
-        ans:"https://codesandbox.io/p/sandbox/string-2-8yg7cy"
+    category: "string",
+    ans: "https://codesandbox.io/p/sandbox/string-2-8yg7cy"
+  },
+  {
+    que: "Write a JS function to count the number of each character in string",
+    code: `
+    function countCharacters(str) {
+      let obj = {}
+      str
+        .split("")
+        .forEach(element => {
+          if (!obj[element]) {
+            obj[element] = 1
+          } else {
+            obj[element] += 1
+          }
+        })
+    
+      return obj
     }
+    
+    console.log(countCharacters("hello world"));
+    `,
+    category: "string"
+  },
+  {
+    que: ' Write a JavaScript function to truncate a string if it is longer than the specified number of characters. Truncated strings will end with a translatable ellipsis sequence ("...") (by default) or specified characters.',
+    code: `
+    function text_truncate(str, number = 0, ending = "---") {
+
+      if (number) {
+        return str.slice(0, number + ending.length) + ending
+      }
+      return str
+    }
+    text_truncate('We are doing JS string exercises.', 19, "...")
+    `,
+    category: "string"
+  },
+  {
+   que:"Write a JavaScript function to chop a string into chunks of a given length.",
+   category:"string",
+   code:`function string_chop(str, num){
+    let arr = []
+  
+    for(let i = 0; i < str.length; i+=num){
+     arr.push(str.slice(i, i+num))   
+    }
+    return arr
+}
+
+string_chop('w3resource',2)` 
+  }
+
 
 
 ]
+
+
+
 
 export default codingQuestion
