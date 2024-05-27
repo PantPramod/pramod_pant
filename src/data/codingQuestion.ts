66f48ceaa1fa6ec6d4f1781d16812bffc4310220
Pramod Pant <pramodpant100@gmail.com>
2024-05-27 11:05:43 +0530

    "Added new coding questions to data file and updated Questions component to display them with copy code functionality"

--- src/data/codingQuestion.ts
+++ src/data/codingQuestion.ts
@@ -298,13 +298,261 @@ string_chop('w3resource',2)`
     isEndsWith('JS PHP PYTHON','PYTHON')
     `,
     category: "string"
+  },
+  {
+    que: "Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.",
+    category: "array",
+    code: `
+    function getFirstNElements(arr, n){
+      if(!n && arr.length){ 
+        return arr[0]
+      }
+      else if(!n && !arr.length){
+        return []
+      }  
+      return arr.slice(0, n)
+    }
+    
+    getFirstNElements([1,2,3,4,5,6,7], 4 )`
+  },
+  {
+    que: "Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.",
+    category: "array",
+    code: `function getLastNElements(arr, n) {
+      if (!n && arr.length) {
+        return arr[arr.length - 1]
+      }
+      else if (!n && !arr.length) {
+        return []
+      } else {
+         return arr.slice(arr.length - n)  
+      }
+    }
+    
+    getLastNElements([1,2,3,4,5,6], 2)`
+  },
+  {
+    que: `Write a simple JavaScript program to join all elements of the following array into a string.
+    Sample array : myColor = ["Red", "Green", "White", "Black"];
+    Expected Output :
+    "Red, Green, White, Black"`,
+    category: "array",
+    code: `
+    function joinArray(arr){
+      return arr.join(', ')
+    }
+    
+    joinArray(["Red", "Green", "White", "Black"])
+    `
+  },
+  {
+    que: "Write a JavaScript program that accepts a number as input and inserts dashes (-) between each even number. For example if you accept 025468 the output should be 0-254-6-8.",
+    category: "array",
+    code: `
+    function insertDashes(num){
+      let arr = num.toString().split('')
+     let newArr=[]
+      for(let i=1; i<arr.length; i++){
+        
+        if( Number(arr[i])%2===0 && Number(arr[i-1])%2===0){
+          newArr.push('-')
+        }
+        newArr.push(arr[i])
+      }
+      return newArr.join('')
+    }
+    
+    insertDashes(1245362982786987424)`
+  },
+  {
+    que: "Write a JavaScript program to sort the items of an array",
+    category: "array",
+    code: `
+    function sortArray(arr){
+      return arr.sort((a,b)=>a-b)
+    }
+
+    sortArray([7,8, -1, 0, 4, 3, 9, 10, 57, 23])
+    `
+  },
+  {
+    que: `Write a JavaScript program to find the most frequent item in an array. 
+    Sample array : var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+    Sample Output : a ( 5 times )`,
+    category: "array",
+    code: `
+    function findMostFrequentItem(arr){
+      let result , frequency=0;
+      let obj={}
+      for(let i=0; i<arr.length; i++){
+         if(!obj[arr[i]]){
+          obj[arr[i]]=1
+         }else{
+          obj[arr[i]]+=1
+         }
+      }
+    
+      for (key in obj){
+        if(obj[key]>frequency){
+          frequency=obj[key];
+          result=key
+        }
+      }
+    
+      return result +"("+ frequency + ") times" 
+    }
+    findMostFrequentItem([12,3,4,5,6,7,7,7,7,7,,7,7,7,7,,8,8,8,6, 1,1,1,1])
+    `
+  },
+  {
+    que: "Function to calculate the sum of squares of elements in an array",
+    category: "array",
+    code: `
+    function sumOfSquares(arr){
+      let sum=0 
+      for(let i=0; i< arr.length; i++){
+         sum+=arr[i]*arr[i] 
+       }
+       return sum
+    }
+    
+    sumOfSquares([0,1,2,3])
+
+    function sumOFSquares(arr){
+      return arr.reduce((acc, cur)=>{
+        return (cur*cur)+acc
+      }, 0)
+    }
+    
+    sumOFSquares([0,1,2,3])
+    
+    `
+  },
+  {
+    que: "Write a JavaScript program to compute the sum and product of an array of integers.",
+    category: "array",
+    code: `
+    function sum(arr){
+      return arr.reduce((acc, cur)=>acc+cur, 0)
+    }
+    function product(arr){
+      return arr.reduce((acc, cur)=>{
+        return acc*cur}
+      )
+    }
+    sum([1,2,3,4,5])
+    
+    product([1,2,3,4])
+    `
+  },
+  {
+    que: "Write a JavaScript program to remove duplicate items from an array",
+    category: "array",
+    code: `
+    function removeDuplicate(arr){
+      return [...new Set(arr)]
+  }
+  
+  removeDuplicate([1,1,22,3,3,4,4,7,5,5,7, 22 ])
+
+  function removeDuplicate(arr){
+    let obj={}
+    
+    for(let i=0;i<arr.length;i++){
+      obj[arr[i]]=0
+    }
+  
+    return Object.keys(obj)
+  
+  }
+  
+  removeDuplicate([1,1,2,2,3,3,3,4,4,5,5,6,6,7,7,8,8,9])
+    `
+  },
+  {
+    que: `Write a JavaScript function to remove. 'null', '0', '""', 'false', 'undefined' and 'NaN' values from an array.`,
+    category: "array",
+    code: `
+    function removeFalseValues(arr){
+     let newArr =[]
+     for(let i=0; i<arr.length; i++){
+          if(arr[i]){
+          newArr.push(arr[i])
+            }
+         }
+      return newArr
+    }
+
+    removeFalseValues( [NaN, 0, 15, false,0, -22, '',undefined, 47, null])
+    `
+  },
+  {
+    que: `Write a JavaScript code to sort the following array of objects by title value.
+    Sample object :
+    
+    var library = [ 
+       { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+       { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+       { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+       ];`,
+    category: "array",
+    code: `
+   var library = [ 
+    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+    { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+    ];
+  
+  
+    library.sort((a,b)=>{
+      let titleA= a.title.toUpperCase()
+      let titleB=b.title.toUpperCase()
+  
+      if(titleA<titleB){
+        return -1
+      }
+      else{
+        return 1
+      }
+    })
+   `
+  },
+  {
+    que: `Write a JavaScript program to find a pair of elements (indices of the two numbers) in a given array whose sum equals a specific target number.
+
+    Input: numbers= [10,20,10,40,50,60,70], target=50
+    Output: 0, 3
+    `,
+    category: "array",
+    code: `
+    function twoSum(arr, target){
+      for(let i=0; i<arr.length; i++){
+        if(arr.includes(target-arr[i])){
+          return [i, arr.indexOf(target-arr[i])]
+        }
+      }
+    }
+    
+    twoSum([10,20,10,40,50,60,70],50)
+
+    
+function twoSum(arr, target){
+  for(let i=0; i<arr.length; i++){
+    
+    for(let j=i+1; j<arr.length; j++){
+      if(arr[i]+arr[j]===target){
+         return[i, j]
+      }
+    }
+  }
+}
+
+twoSum([10,20,10,40,50,60,70],50)
+
+    `
   }
 
-
-
 ]
-
-
 
 
 
--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -3,38 +3,60 @@ import { useParams } from "react-router-dom"
 import CodeComponent from "../components/Code/CodeComponent"
 import codingQuestion from "../data/codingQuestion"
 import { RxCodesandboxLogo } from "react-icons/rx"
+import { RiFileCopyLine } from "react-icons/ri";
+import { useEffect, useState } from "react";
 
 const Questions = () => {
     const { id } = useParams()
+    const que = codingQuestion.filter((item) => item.category === id)
+    const [showCopyMessage, setShowCopyMessage] = useState(false)
+    const [copyId, setCopyId] = useState(-1)
+    const copyCode = (txt: string, id: number) => {
+        setCopyId(id)
+        setShowCopyMessage(true)
+        navigator.clipboard.writeText(txt)
+    }
+
+    useEffect(() => {
+        let timerId: any
+        if (showCopyMessage) {
+            timerId = setTimeout(() => {
+                setShowCopyMessage(false)
+            }, 1000)
+        }
+
+        return () => {
+           timerId && clearTimeout(timerId)
+        }
+    }, [showCopyMessage])
     return (
         <div className="w-[90%] mx-auto">
             <p className="text-center text-2xl md:text-3xl font-bold mb-4 text-gray-700 py-5 capitalize">{id} {" "} Questions</p>
 
             <div className="py-10 ">
-                {
-                    codingQuestion.map((item, index) => <div key={item?.que} className="">
-                        {id === item.category
-                            &&
-                            <>
-                                <p className="py-10"><strong>{index + 1}.</strong>{" "}{item?.que}
-                                    {
-                                        item?.ans &&
-                                        <a href={item?.ans} target="_blank">
-                                            <RxCodesandboxLogo
-                                                size={22}
-                                                color="blue"
-                                                className="inline-block ml-2 cursor-pointer"
-                                            />
-                                        </a>
-                                    }
-
-                                </p>
-
-                                <CodeComponent code={item?.code} />
-
-                            </>
+                {que.map((item, index) => <div key={item?.que} className="">
+                    <p className="py-10"><strong>{index + 1}.</strong>{" "}{item?.que}
+                        {
+                            item?.ans &&
+                            <a href={item?.ans} target="_blank">
+                                <RxCodesandboxLogo
+                                    size={22}
+                                    color="blue"
+                                    className="inline-block ml-2 cursor-pointer"
+                                />
+                            </a>
                         }
-                    </div>)
+                    </p>
+
+                    <div
+                        onClick={() => copyCode(item?.code, index)}
+                        className="relative cursor-pointer w-7 h-7 flex items-center justify-center bg-gray-200 rounded-md ml-auto mr-2">
+                        {showCopyMessage && (index === copyId) && <span className="absolute right-[120%] text-green-600">Copied</span>}
+                        <RiFileCopyLine size={22} className="text-gray-700" />
+                    </div>
+
+                    <CodeComponent code={item?.code} />
+                </div>)
                 }
             </div>
         </div>
